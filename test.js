b26b4fc387d63c630e88285b0cbf54b4742d9c06
fintie <fintie@gmail.com>
2013-10-29 11:06:17 +1100

    Update test.js

--- test.js
+++ test.js
@@ -271,7 +271,10 @@ app.post('/generateexisting', function(req, res){
 
 app.post('/updateactivity', function (req, res){
 
-	customerid = req.body.code;
+	if(customerid = "")
+	{
+		customerid = req.body.code;
+	}	
 	//var classification = new Array();
 	var output = '<html><body><form name="input" action="/regenerate" method="post">';
 	output += 'Customer ID:' + customerid + '<br>'; 
