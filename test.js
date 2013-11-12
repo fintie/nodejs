e2abc9d4cad8e0ab8d2e31702404a4cd02392dd7
Nicholas Qi <fintie@gmail.com>
2013-11-12 15:16:12 +1100

    final test

--- test.js
+++ test.js
@@ -239,7 +239,7 @@ app.post('/generate', function (req, res){
 		console.log('INSERT INTO estimates (method, price) VALUES ("' + method +'","' + price + '") WHERE ID = "' + estimateid + '";');
 		connection.query('INSERT INTO estimates (method, price) VALUES ("' + method +'","' + price + '") WHERE ID = "' + estimateid + '";');
 */
-		limit = number
+		limit = number;
 		for (var i=0; i<number; i++){
 			if(number==1){
 				console.log('INSERT INTO activity (EstimateID, Status, Classification, Hours, Deadline) values ("'+ estimateid + '","Generated", "' + classification +'","' + hours + '","' + deadline +'");');
