52e6ca47648127365fa31a14d73a03218455ebd8
Nicholas Qi <fintie@gmail.com>
2013-11-12 15:36:07 +1100

    add datetime

--- test.js
+++ test.js
@@ -242,13 +242,13 @@ app.post('/generate', function (req, res){
 		limit = number;
 		for (var i=0; i<number; i++){
 			if(number==1){
-				console.log('INSERT INTO activity (EstimateID, Status, Classification, Hours, Deadline) values ("'+ estimateid + '","Generated", "' + classification +'","' + hours + '","' + deadline +'");');
-				connection.query('INSERT INTO activity (EstimateID, Status, Classification, Hours, Deadline) values ("'+ estimateid + '","Generated", "' + classification +'","' + hours + '","' + deadline +'");');
+				console.log('INSERT INTO activity (EstimateID, Status, DateTime, Classification, Hours, Deadline) values ("'+ estimateid + '","Generated", NOW(), "' + classification +'","' + hours + '","' + deadline +'");');
+				connection.query('INSERT INTO activity (EstimateID, Status, DateTime, Classification, Hours, Deadline) values ("'+ estimateid + '","Generated", NOW(), "' + classification +'","' + hours + '","' + deadline +'");');
 
 			}
 			else{
-				console.log('INSERT INTO activity (EstimateID, Status, Classification, Hours, Deadline) values ("'+ estimateid + '","Generated", "' + classification[i] +'","' + hours[i] + '","' + deadline[i] +'");');
-				connection.query('INSERT INTO activity (EstimateID, Status, Classification, Hours, Deadline) values ("'+ estimateid + '","Generated", "' + classification[i] +'","' + hours[i] + '","' + deadline[i] +'");');
+				console.log('INSERT INTO activity (EstimateID, Status, DateTime, Classification, Hours, Deadline) values ("'+ estimateid + '","Generated", NOW(), "' + classification[i] +'","' + hours[i] + '","' + deadline[i] +'");');
+				connection.query('INSERT INTO activity (EstimateID, Status, DateTime, Classification, Hours, Deadline) values ("'+ estimateid + '","Generated", NOW(), "' + classification[i] +'","' + hours[i] + '","' + deadline[i] +'");');
 			}
 			//insert milestone
 			console.log('INSERT INTO milestones ( EstimateID) values (' + "'" + estimateid +"'" +');');
