ec757e130d14e0bb1facfc1153495299e3c3fef7
Nicholas Qi <fintie@gmail.com>
2013-11-13 16:37:06 +1100

    Get only the last activity ID

--- test.js
+++ test.js
@@ -424,7 +424,7 @@ app.post('/customerapproval', function (req, res){
 
 app.post('/approve', function (req, res){
 	console.log(estimateid);
-	connection.query('SELECT * FROM activity WHERE EstimateID = '+ estimateid +';' , function (error, rows, fields) { 
+	connection.query('SELECT * FROM activity WHERE EstimateID = '+ estimateid +' ORDER BY ID DESC LIMIT 1;' , function (error, rows, fields) { 
 		milestoneid = rows[0].MilestoneID;
 		classification = rows[0].Classification;
 		hours = rows[0].Hours;
@@ -507,7 +507,7 @@ app.post('/updatetime', function (req, res){
 });
 
 app.get('/checklist', function (req,res){
-	connection.query("SELECT MilestoneID FROM activity WHERE Status= 'Executed';", function (error, rows, fields) {
+	connection.query("SELECT MilestoneID FROM activity WHERE Status= 'Executed' ORDER BY ID DESC LIMIT 1;", function (error, rows, fields) {
 			var output = '<html><head></head><body><form name="input" action="/displaymilestone" method="post"><select name="milestone">';
 			for (var i in rows) {
 				output += '<option value=' + rows[i].MilestoneID + '>' + rows[i].MilestoneID + '</option>';
@@ -623,7 +623,7 @@ app.post('/updatestatus', function(req,res){
 });
 
 app.get('/selectchecked', function (req,res){
-	connection.query("SELECT MilestoneID FROM activity WHERE Status= 'Checked';", function (error, rows, fields) {
+	connection.query("SELECT MilestoneID FROM activity WHERE Status= 'Checked' ORDER BY ID DESC LIMIT 1;", function (error, rows, fields) {
 			var output = '<html><head></head><body><form name="input" action="/displaymilestone" method="post"><select name="milestone">';
 			for (var i in rows) {
 				output += '<option value=' + rows[i].MilestoneID + '>' + rows[i].MilestoneID + '</option>';
@@ -635,7 +635,7 @@ app.get('/selectchecked', function (req,res){
 });	
 
 app.get('/billing', function (req,res){
-	connection.query("SELECT MilestoneID FROM activity WHERE Status= 'Complete';", function (error, rows, fields) {
+	connection.query("SELECT MilestoneID FROM activity WHERE Status= 'Complete' ORDER BY ID DESC LIMIT 1;", function (error, rows, fields) {
 			var output = '<html><head></head><body><form name="input" action="/displaymilestone" method="post"><select name="milestone">';
 			for (var i in rows) {
 				output += '<option value=' + rows[i].MilestoneID + '>' + rows[i].MilestoneID + '</option>';
@@ -647,7 +647,7 @@ app.get('/billing', function (req,res){
 });	
 
 app.get('/customerinvoice', function (req,res){
-	connection.query("SELECT MilestoneID FROM activity WHERE Status= 'Invoiced';", function (error, rows, fields) {
+	connection.query("SELECT MilestoneID FROM activity WHERE Status= 'Invoiced' ORDER BY ID DESC LIMIT 1;", function (error, rows, fields) {
 		var output = '<html><head></head><body><form name="input" action="/displaymilestone" method="post"><select name="milestone">';
 		for (var i in rows) {
 			output += '<option value=' + rows[i].MilestoneID + '>' + rows[i].MilestoneID + '</option>';
