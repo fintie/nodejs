498497eaa9c711046a3d47e0fff048424233078c
Nicholas Qi <fintie@gmail.com>
2013-11-14 15:54:43 +1100

    get the last activity ID for milestone ID when selecting

--- test.js
+++ test.js
@@ -521,8 +521,8 @@ app.get('/checklist', function (req,res){
 app.post('/displaymilestone', function(req,res){
 
 	milestoneid = req.body.milestone;
-	console.log('SELECT e.CustomerID, a.EstimateID, a.Status, a.Producer, a.Classification, a.Hours, a.Trigger, a.Deadline, a.Proportion FROM estimates e, activity a WHERE e.ID = a.EstimateID AND a.MilestoneID="'+ milestoneid +'" ORDER BY a.ID DESC LIMIT 1;');
-	connection.query('SELECT e.CustomerID, a.EstimateID, a.Status, a.Producer, a.Classification, a.Hours, a.Trigger, a.Deadline, a.Proportion FROM estimates e, activity a WHERE e.ID = a.EstimateID AND a.MilestoneID="'+ milestoneid +'" ORDER BY a.ID DESC LIMIT 1;', function (error, rows, fields) {
+	console.log('SELECT e.CustomerID, a.EstimateID, a.Status, a.Producer, a.Classification, a.Hours, a.Trigger, a.Deadline, a.Proportion FROM estimates e, activity a WHERE a.ID = any (SELECT MAX(ID) FROM activity GROUP BY MilestoneID) AND e.ID = a.EstimateID AND a.MilestoneID="'+ milestoneid +'";');
+	connection.query('SELECT e.CustomerID, a.EstimateID, a.Status, a.Producer, a.Classification, a.Hours, a.Trigger, a.Deadline, a.Proportion FROM estimates e, activity a WHERE a.ID = any (SELECT MAX(ID) FROM activity GROUP BY MilestoneID) AND e.ID = a.EstimateID AND a.MilestoneID="'+ milestoneid +'";', function (error, rows, fields) {
 			customerid = rows[0].CustomerID;
 			estimateid = rows[0].EstimateID;
 			producer = rows[0].Producer;
