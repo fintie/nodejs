010c8b18145e889a803a3c24106644a9ac64c635
Nicholas Qi <fintie@gmail.com>
2013-11-26 14:54:40 +1100

    group by milestone ID

--- test.js
+++ test.js
@@ -309,12 +309,13 @@ app.post('/generateexisting', function(req, res){
 			*/
 	});
 
-	console.log('SELECT e.CustomerID, a.MilestoneID, a.Classification, a.Hours, a.Trigger, a.Deadline, a.Proportion FROM estimates e, activity a WHERE e.ID = a.EstimateID AND a.EstimateID = "'+ estimateid +'";');
-	connection.query('SELECT e.CustomerID, a.MilestoneID, a.Classification, a.Hours, a.Trigger, a.Deadline, a.Proportion FROM estimates e, activity a WHERE e.ID = a.EstimateID AND a.EstimateID = "'+ estimateid +'";', function (error, rows, fields) {
+	console.log('SELECT e.CustomerID, a.MilestoneID, a.Classification, a.Hours, a.Trigger, a.Deadline, a.Proportion FROM estimates e, activity a WHERE e.ID = a.EstimateID AND a.EstimateID = "'+ estimateid +'" GROUP BY a.MilestoneID;');
+	connection.query('SELECT e.CustomerID, a.MilestoneID, a.Classification, a.Hours, a.Trigger, a.Deadline, a.Proportion FROM estimates e, activity a WHERE e.ID = a.EstimateID AND a.EstimateID = "'+ estimateid +'" GROUP BY a.MilestoneID;', function (error, rows, fields) {
 			
 		for (var i in rows) {
 			customerid = rows[i].CustomerID;
 			milestoneid = rows[i].MilestoneID;
+			console.log(milestoneid);
 			classification = rows[i].Classification;
 			hours = rows[i].Hours;
 			trigger = rows[i].Trigger;
